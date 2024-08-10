37ed00501ed05cd73813a51009f0f16f3748159d
parrizasDeveloper <maparrizasd@gmail.com>
2024-08-10 15:32:42 +0200

    1.5

--- part1/infoCourse/src/App.jsx
+++ part1/infoCourse/src/App.jsx
@@ -24,44 +24,36 @@ function Part({title, exercises}) {
   )
 }
 
-function Total({exercises}) {
-  const total = exercises.reduce((sum, exercise) => (
-    sum += exercise
-  ))
+function Total({parts}) {
+  const total = parts.reduce((sum, part) => sum += part.exercises, 0)
   return (
     <p>Number of exercises: {total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        title: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        title: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        title: 'State of a component',
+        exercises: 14
+      }]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-        parts={[
-          {
-            title: part1,
-            exercises: exercises1
-          },
-          {
-            title: part2,
-            exercises: exercises2
-          },
-          {
-            title: part3,
-            exercises: exercises3
-          }
-        ]} 
-      />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
